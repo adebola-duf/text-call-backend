7723268941322d6e407688dc9f04e798819ae6dc
Odufuwa Adebola <adeboladuf@gmail.com>
2024-04-19 18:20:57 +0100

    Revert "Changed the content of message being sent."
    This reverts commit 1fc53d8222e392bdee7c522e5d4079b9fa49829d.

--- main.py
+++ main.py
@@ -42,24 +42,15 @@ class CallData(BaseModel):
     caller_phone_number: str
     callee_phone_number: str
     message: str
-    background_color: 'Color'
-
-
-class Color(BaseModel):
-    red: int
-    blue: int
-    green: int
-    alpha: int
+    background_color: dict
 
 
 @app.get(path="/call/{call_status}/{caller_phone_number}")
-async def handle_call(call_status: str, caller_phone_number: str):
+async def handle_call(call_status: str,caller_phone_number: str):
     if call_status == 'rejected':
-        data = {'message': 'I doth decline the call with utmost regret.',
-                'call_status': call_status}
+        data = {'message': 'I doth decline the call with utmost regret.', 'call_status': call_status}
     elif call_status == 'accepted':
-        data = {'message': 'I hath indeed heeded the beckoning of the telephone.',
-                'call_status': call_status}
+        data = {'message': 'I hath indeed heeded the beckoning of the telephone.', 'call_status': call_status}
 
     await manager.send_to(caller_phone_number=caller_phone_number, data=data)
 
@@ -68,10 +59,11 @@ class ConnectionManager:
     def __init__(self):
         # this dictionary is to store phone number - websocket objects mappings
         self.caller_phone_number_websocket_dict: dict[str, WebSocket] = {}
-
+    
     async def connect(self, websocket: WebSocket, caller_phone_number: str):
         await websocket.accept()
         self.caller_phone_number_websocket_dict[caller_phone_number] = websocket
+
 
     def disconnect(self, websocket: WebSocket):
         phone_number_websocket_connection_to_delete = None
@@ -81,12 +73,10 @@ class ConnectionManager:
         del self.caller_phone_number_websocket_dict[phone_number_websocket_connection_to_delete]
 
     async def send_to(self, caller_phone_number: str, data: dict):
-        if (caller_phone_number in self.caller_phone_number_websocket_dict):
-            await self.caller_phone_number_websocket_dict[caller_phone_number].send_json(data)
+        await self.caller_phone_number_websocket_dict[caller_phone_number].send_json(data)
 
 
 manager = ConnectionManager()
-
 
 @app.websocket("/ws/{caller_phone_number}")
 async def websocket_endpoint(websocket: WebSocket, caller_phone_number: str):
@@ -97,14 +87,13 @@ async def websocket_endpoint(websocket: WebSocket, caller_phone_number: str):
             call_data = CallData.model_validate(data)
             print(call_data)
 
-            doc_ref = db.collection("users").document(
-                call_data.callee_phone_number)
+            doc_ref = db.collection("users").document(call_data.callee_phone_number)
             doc = await doc_ref.get()
-
+          
             document = doc.to_dict()
             print(f"Document data: {document}")
             message = messaging.Message(
-                notification=messaging.Notification(title=f'{call_data.caller_phone_number} is calling'), data={'message': call_data.message, 'caller_phone_number': call_data.caller_phone_number, 'red': call_data.background_color.red, 'blue': call_data.background_color.blue, 'green': call_data.background_color.green, 'alpha': call_data.background_color.alpha}, token=document['fcmToken'],)
+                notification=messaging.Notification(title=f'{call_data.caller_phone_number} is calling'),data={'message': call_data.message, 'caller_phone_number': call_data.caller_phone_number, 'background_color': json.dumps(call_data.background_color)}, token=document['fcmToken'],)
 
             # Send a message to the device corresponding to the provided registration token.
             response = messaging.send(message)
